bbeaa24fad69cedf44a34e7312fcd41ffbc16c6b
Maros Zatko <hacxman@gmail.com>
2013-07-02 01:54:34 +0200

    build.sh: deslicer->src

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 tar czhvf deslicer-0.0.1.tar.gz deslicer-0.0.1
 cp deslicer-0.0.1.tar.gz ~/rpmbuild/SOURCES/deslicer-0.0.1.tar.gz
-pushd deslicer
+pushd src
 rpmbuild -bs deslicer.spec
 popd
 mock -r fedora-18-x86_64 --rebuild ~/rpmbuild/SRPMS/deslicer-0.0.1-1.fc18.src.rpm
